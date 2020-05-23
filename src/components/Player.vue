cfc979fe0b631605c0dc0d07218ed790c9e6b773
Walt Thiessen <walt@vahost.net>
2020-05-23 16:24:21 -0400

    backup

---
+++ src/components/Player.vue
@@ -0,0 +1,23 @@
+<template>
+  <vue-plyr>
+    <audio>
+      <source src="https://www.loatoday.net/podlove/file/3494/s/download/c/select-show/2020/loa1225-working-with-the-law-11-truth-principles-for-successful-living-by-raymond-holliwell-part-5.mp3" type="audio/mp3"/>
+    </audio>
+  </vue-plyr>
+</template>
+
+<script>
+import Vue from 'vue'
+import VuePlyr from 'vue-plyr'
+
+// The second argument is optional and sets the default config values for every player.
+Vue.use(VuePlyr, {
+  plyr: {
+    fullscreen: { enabled: false }
+  },
+  emit: ['ended']
+})
+export default {
+  name: 'Player'
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex-center">
+    <player/>
+  </q-page>
+</template>
+
+<script>
+import VueRssFeed from "vue-rss-feed"
+export default {
+  data(){
+    return{}
+  },
+  components: {
+    'player' : require('components/Player.vue').default
+  }
+}
+</script>
