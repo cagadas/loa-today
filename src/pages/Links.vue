9bde4d355195d35b575025b17fec78baabeaa2f9
Walt Thiessen <walt@vahost.net>
2020-06-27 08:46:40 -0400

    Links remove target and update Android

--- src/pages/Links.vue
+++ src/pages/Links.vue
@@ -3,7 +3,7 @@
     <h4 class="center">Links To Web Pages And Apps</h4>
     <div style="margin: 0 auto;width: 280px;">
       <div>
-        <a target="_blank" href="https://www.amazon.com/Your-Daily-Dose-Happy-Attraction-ebook/dp/B07D6DR3RV/ref=sr_1_2?dchild=1&amp;keywords=your+daily+dose+of+happy&amp;qid=1593257176&amp;s=books&amp;sr=1-2&_encoding=UTF8&tag=professiona07-20&linkCode=ur2&linkId=dce69a0475f1892958336cc259c941c2&camp=1789&creative=9325">
+        <a href="https://www.amazon.com/Your-Daily-Dose-Happy-Attraction-ebook/dp/B07D6DR3RV/ref=sr_1_2?dchild=1&amp;keywords=your+daily+dose+of+happy&amp;qid=1593257176&amp;s=books&amp;sr=1-2&_encoding=UTF8&tag=professiona07-20&linkCode=ur2&linkId=dce69a0475f1892958336cc259c941c2&camp=1789&creative=9325">
           <img class="image" src="images/your-daily-dose-of-happy.jpg" alt="Your Daily Dose Of Happy: Real Life Success Stories Of The Law Of Attraction">
         </a>
       </div>
