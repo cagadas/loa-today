11a527a51b826c15a7f4510cea3c78ee1a741383
Walt Thiessen <walt@vahost.net>
2020-06-29 14:13:03 -0400

    schedule

--- src/pages/Schedule.vue
+++ src/pages/Schedule.vue
@@ -1,14 +1,20 @@
 <template>
   <div style="text-align: center;">
-    <h3>Our Podcast Recording Schedule</h3>
-    <h4>All show times are in the Eastern Time zone (New York)</h4>
-    <p>Monday, 4pm - 5pm (with Louis de Souza, Uohna June Thiessen, &amp; Walt Thiessen)</p>
+    <h4>Our Podcast Recording Schedule</h4>
+    <h5>All show times are in the Eastern Time zone (New York)</h5>
+    <p>&nbsp;</p>
+    <p>Monday, 4pm - 5pm (with Louis de Souza &amp; Walt Thiessen)</p>
     <p>Tuesday, 4pm - 5pm (with Dan Mangena, Alyx King  &amp; Walt Thiessen)</p>
     <p>Wednesday, 4pm - 5pm (with Cindie Chavez &amp; Walt Thiessen)</p>
     <p>Thursday, 4pm - 5pm (with Alyx King, Dan Mangena, &amp; Walt Thiessen)</p>
-    <p>Friday, 4pm - 5pm (with Linda Armstrong, Rita Gigante, &amp; Walt Thiessen)
-
-<h4>Theme Music Copyright: plastic3music / 123RF Stock Photo</h4>
-
+    <p>Friday, 4pm - 5pm (with Linda Armstrong, Rita Gigante, &amp; Walt Thiessen)</p>
+    <p>&nbsp;</p>
+    <p>Theme Music Copyright: <a href="https://www.123rf.com/profile_plastic3music">plastic3music / 123RF Stock Photo</a></p>
   </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
