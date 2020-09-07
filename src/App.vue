4ff6a786b9756f3f30df1e287aa87d68a0aa4702
Walt Thiessen <walt@vahost.net>
2020-09-07 09:53:27 -0400

    v1.1.76

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,6 @@
 </template>
 
 <script>
-import Vue from 'vue'
-import VuePlyr from 'vue-plyr'
-
-// The second argument is optional and sets the default config values for every player.
-Vue.use(VuePlyr, {
-  plyr: {
-    fullscreen: { enabled: false }
-  },
-  options: {
-    controls: [
-      'play',
-      'progress',
-      'current-time',
-      'mute',
-      'volume',
-      'settings'
-    ]
-  },
-  emit: ['ended']
-})
 export default {
   name: 'App',
   /*
